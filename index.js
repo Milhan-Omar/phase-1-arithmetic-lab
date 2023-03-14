2abecf9abcf7a9043b9e95d493d8c05e674939a4
milhan omar <milhanomarosman@gmail.com>
2023-03-14 15:21:58 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// const multiply = (results) => {
+//  results = num1 * num2
+// }
+// const multiply = "num1 * num2"
+// let Number;
+// function random(Number){ 
+//  if(random < 0)
 
+// }
+// let rdNumbers = "2,5,15,19,14,4,9,20"
+// rdNumbers.max()
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1* num2;
+let random = 56;
+Number.isInteger(true);
+let max =20;
+Math.max=(2,4,6,8,10,12,14,16,18,20);
+max;
+let num3 = 9;
+let num4 = 5;
+let mod = num3 - num4;
+mod;
